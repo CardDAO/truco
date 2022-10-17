4a93badff27a770616c68e1f5f5a33220ffc65d3
Julian Paredes <jparedes@gmail.com>
2022-10-17 11:25:55 -0300

    Add protocol basic scaffolding

---
+++ protocol/hardhat.config.ts
@@ -0,0 +1,34 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-etherscan"
+
+import "dotenv/config";
+
+const { REPORT_GAS, COINMARKETCAP_KEY }  = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.16"
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  gasReporter: {
+    enabled: REPORT_GAS == 'true',
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_KEY,
+    token: 'ETH',
+    gasPriceApi: 'https://api.bscscan.com/api?module=proxy&action=eth_gasPrice',
+    showTimeSpent: true,
+  },
+};
+
+export default config;
