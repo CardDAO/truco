61a9c7d836e5d23633fc23a03592a0768503da15
Julian Murphy <njmdistrisoft@gmail.com>
2022-10-19 20:01:53 -0300

    tailwind install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import logo from './logo.svg'
 import './App.css'
+import { Navbar } from './components/Navbar'
 
 
 function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <Navbar />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+        <h1 className="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
     );
 }
 
