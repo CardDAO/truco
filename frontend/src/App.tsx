529cbaafdbe55eec7052b818a90dc9b3fbe0df9c
Julian Murphy <njmdistrisoft@gmail.com>
2022-10-20 19:04:29 -0300

    game component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
+import { Game } from './components/Game'
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <Game />
         </div>
     );
 }
