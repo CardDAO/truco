7fb0bc7629896dc0530dbc9a0753c9d113eb0ddc
Julian Murphy <njmdistrisoft@gmail.com>
2022-10-20 20:59:11 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
-import { Navbar } from './components/Navbar'
-import { Game } from './components/Game'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './hooks/router'
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <Game />
-        </div>
+        <RouterProvider
+            router={router}
+            fallbackElement={
+                <h1 className="text-4xl text-white">Loading</h1>
+            }
+        />
     );
 }
 
