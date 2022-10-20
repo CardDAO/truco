1922cd993bacd4c7c6a2af8b9ce2d07b29cede6e
Julián Paredes <julpar@users.noreply.github.com>
2022-10-20 08:55:52 -0300

    Solidity foundation (#4)
    * Add common structs proposal
    
    * WIP: First deck encryption/decryption logic
    
    * Restrict index parameter length
    
    * Use memory for accessing instead of storage
    
    * more optimization: read from storage once
    
    * Bug: was not using XOR but OR
    
    * Fixes @fkmurphy review comments
    
    * Prettier

---
+++ protocol/contracts/trucoV1/Structs.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+uint8 constant CardsCount = 40;
+
+library CardsStructs {
+    struct Deck {
+        bytes1[CardsCount] cards;
+    }
+
+    enum Action {
+        PlayCard,
+        Challenge,
+        Response,
+        Resign
+    }
+
+    enum Challenge {
+        Truco,
+        ReTruco,
+        ValeCuatro,
+        Envido,
+        EnvidoEnvido,
+        RealEnvido,
+        FaltaEnvido
+    }
+
+    enum Response {
+        Accept,
+        DontAccept,
+        EnvidoCount
+    }
+
+    struct Move {
+        Action action;
+        bytes1[] parameters;
+    }
+}
