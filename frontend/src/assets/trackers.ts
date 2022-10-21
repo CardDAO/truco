37bd737d0aef83f0b06520a6463a244c5e7c3580
Julian Murphy <njmdistrisoft@gmail.com>
2022-10-21 01:40:47 -0300

    peers

---
+++ frontend/src/assets/trackers.ts
@@ -0,0 +1,6 @@
+export const trackersURL = [
+  "wss://tracker.openwebtorrent.com",
+  "wss://tracker.sloppyta.co:443/announce",
+  "wss://tracker.novage.com.ua:443/announce",
+  "wss://tracker.btorrent.xyz:443/announce",
+]
--- frontend/src/components/Game/index.tsx
+++ frontend/src/components/Game/index.tsx
@@ -1,3 +1,43 @@
+import { useState } from 'react'
+import P2PT from "p2pt"
+import {  useAccountInformation, AccountType, useCreateRoom, Player, Message, sendMessage } from '../../hooks/providers/Wagmi'
+
 export const Game = () => {
-    return <h1 className="text-white text-4xl text-center align-middle">Start game</h1>
+
+    const { address, isConnected } : AccountType = useAccountInformation()
+    const [ players, setPlayers ] = useState<Player[]>([])
+
+    const p2pt = useCreateRoom(
+        (newPlayer: Player) => {
+            console.log('player', newPlayer)
+            setPlayers(currentPlayers => [...currentPlayers, newPlayer])
+        },
+        (message: Message) => {
+            console.log('recibí', message)
+        }
+    )
+
+    const sendMessageClick = () => {
+        console.log(p2pt)
+        sendMessage(
+            p2pt as P2PT<Message>,
+            {peer: players[0], message: {message: "Holis"}},
+            (message: Message) => {
+                console.log('respuesta', message)
+            }
+        )
+    }
+    return (
+        <>
+            {
+                isConnected ? 
+                    <div>
+                        <h1 className="text-white text-4xl text-center align-middle">Start game {address}</h1>
+                        <button onClick={() => {sendMessageClick()}}>Send message</button>
+                    </div>
+                    :
+                    <h1 className="text-white text-4xl text-center align-middle">Connect wallet</h1>
+            }
+        </>
+    )
 }
