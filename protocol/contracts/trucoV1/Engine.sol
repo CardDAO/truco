4d6a244b6bcee6e4d2914c30442f03b37b13484f
Julian Paredes <jparedes@gmail.com>
2022-10-24 19:29:48 -0300

    Some polishing

--- protocol/contracts/trucoV1/Engine.sol
+++ protocol/contracts/trucoV1/Engine.sol
@@ -73,34 +73,40 @@ contract CardsEngine is Ownable {
             return _gameState;
         }
 
-        // Aca se puede asumir que el challenge no esta esperando una rta
+        // Check if an Envido is being played or challenged, in that case this should be the priority
         if (isChallengeAnEnvido(_gameState.currentChallenge)) {
 
+            // Accepted moves up to this point are enforced here
             require(_move.action == CardsStructs.Action.Challenge || _move.action == CardsStructs.Action.Response  || _move.action == CardsStructs.Action.EnvidoCount );
 
+            // 3 possible cases are handled:
 
-            // 3 casos
+            // 1) Check if action is a challenge "rising" (i.e: Envido -> Real Envido! / Envido -> Falta Envido!)
+            if (_move.action == CardsStructs.Action.Challenge ) {
+                // Preconditions:
+                // - Previous challenge was accepted
+                // - Player is not the challenger
+                // - No points were previously spoken
+                // - Haven't reach maximum challenges spelled
+                require (_gameState.currentChallenge.response == CardsStructs.Response.Accept);
+                require (_gameState.playerTurn != _gameState.currentChallenge.challenger );
+                require (_gameState.envidoCountPerPlayer[0] == 0 && _gameState.envidoCountPerPlayer[1] == 0);
+                //require (no maximium reached);
 
-            // 1) Subo la apuesta. Precondiciones: 
-            // - challenge anterior aceptado.
-            // - no ser el challenger
-            // - no haya puntos cantados
-            // - no haber alcanzado el limite de re-challenges
+            }
 
-            // 2) Respondo la apuesta cantada (Si o no.). Precondiciones:
-            //  - challenge sin respuesta
-            // -  no ser el challenger
-
-            // 3) Canto puntos. precondicion: 
-            //  - challenge con respuesta
-            // - checkear si sos mano o no y en funcion de eso si el otro ya cantó
-
-
+            // 2) Check if action is an envido acceptance or refusal
+            if (_move.action == CardsStructs.Action.Response ) {
+                // Preconditions:
+                // - Challenge should have no response
+                // - Can't be the challenger
+            }
             
-            // Se está esperando una respuesta, no puede venir un count u otra action
-            if (_gameState.currentChallenge.waitingChallengeResponse )  {
-                require (_move.action == CardsStructs.Action.Response);
-                
+            // 3) Envido point spell: 
+            if (_move.action == CardsStructs.Action.EnvidoCount ) {
+                // Preconditions:
+                // - Challenge should have been accepted by other party
+                // - Check if i'm in place for spelling my points (i am "mano") or by contrary it's other players turn
             }
 
 
@@ -135,7 +141,15 @@ contract CardsEngine is Ownable {
         internal
         returns (bool)
     {
-        return true;
+        if (
+            _challenge.challenge == CardsStructs.Challenge.Envido ||
+            _challenge.challenge == CardsStructs.Challenge.RealEnvido ||
+            _challenge.challenge == CardsStructs.Challenge.FaltaEnvido
+        ) {
+            return true; 
+        }
+
+        return false;
     }
 
     function isChallengeATruco(CardsStructs.Challenge _challenge)
