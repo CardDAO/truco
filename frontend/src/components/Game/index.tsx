52b7c661c442ae90de9d023d8971da80d635af82
Julian Murphy <njmdistrisoft@gmail.com>
2022-10-24 20:03:33 -0300

    working p2pt

--- frontend/src/components/Game/index.tsx
+++ frontend/src/components/Game/index.tsx
@@ -4,16 +4,16 @@ import { useTruco } from "../../engine/truco/useTruco"
 
 export const Game = () => {
 
+    const [messageInput, setMessageInput] = useState("")
     const { 
         address,
         isConnected,
-        game,
-        setGame,
         recoveredAddress,
-        signMessage,
-        data,
         clickConnectToGame,
-        inSession
+        inSession,
+        peers,
+        sendMessageAll,
+        messages
     } = useTruco()
 
 
@@ -30,18 +30,31 @@ export const Game = () => {
                     </div>
                     :
                     <div className="text-center align-middle">
-                        <h3 className="text-white text-xl">Jugadores</h3>
+                        <h3 className="text-white text-xl">Peers</h3>
                         <div className="w-100 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                            { game.players.map((player, index) => {
-                                console.log("listando jugador",player, index)
+                            { peers.map((peer, index) => {
+                                console.log("listando peer",peer, index)
                                 return (
                                     <button key={index} type="button" className="py-2 px-4 w-full font-medium text-left border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                                        {player.toString() ?? "undefined"}
+                                        {peer.id ?? "undefined"}
                                     </button>
                                 )
 
                             })}
                         </div>
+                        <h3 className="text-white text-xl">mensajes</h3>
+                        <div className="w-100 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            { messages.map((messageObject, index) => {
+                                return (
+                                    <button key={index} type="button" className="py-2 px-4 w-full font-medium text-left border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                                        {messageObject.message ?? "undefined"}
+                                    </button>
+                                )
+
+                            })}
+                        </div>
+                        <input type="text" value={messageInput} onChange={(event: any) => {setMessageInput(event.target.value)}} />
+                        <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={() => { sendMessageAll(messageInput) }}>Send message</button>
                     </div>
             }
         </>
--- frontend/src/engine/truco/useTruco.tsx
+++ frontend/src/engine/truco/useTruco.tsx
@@ -6,52 +6,98 @@ import { GameInterface } from "../../engine/shared/GameInterface"
 import { useSignMessage } from 'wagmi'
 import { verifyMessage } from 'ethers/lib/utils'
 
-export const useTruco = () => {
+import { trackersURL }  from '../../assets/trackers'
+import P2PT, { Peer } from "p2pt"
 
-    const [ game, setGame ] = useState(new Truco())
+type MessageType = {
+    message: String
+}
+
+
+export const useTruco = () => {
+    //const [ game, setGame ] = useState(new Truco())
+    const [p2pt, setP2PT] = useState(new P2PT<MessageType>(trackersURL, 'UNIQUE_KEY_GAME'))
+
+    const [ peers, setPeers ] = useState([] as Peer[])
     const recoveredAddress = useRef<String>()
     const { address, isConnected } : AccountType = useAccountInformation()
     const [ inSession, setInSession ]  = useState(false)
+    const [ messages, setMessages ] = useState([] as MessageType[])
 
-    const { data, error, isLoading, signMessage } = useSignMessage({
-      onSuccess(data, variables) {
-        // Verify signature when sign message succeeds
-        const address = verifyMessage(variables.message, data)
-        console.log('verifico', data, variables, address)
-        recoveredAddress.current = address
-      },
-    })
+
+   // const { data, error, isLoading, signMessage } = useSignMessage({
+   //   onSuccess(data, variables) {
+   //     // Verify signature when sign message succeeds
+   //     const address = verifyMessage(variables.message, data)
+   //     console.log('verifico', data, variables, address)
+   //     recoveredAddress.current = address
+   //   },
+   // })
+
+    const sendMessageAll = useCallback((message: String) => {
+        if (peers.length > 0) {
+            const miMensaje = {message: p2pt._peerId + ": " + message}
+            setMessages((currentMessages) => [...currentMessages, miMensaje])
+            peers.forEach((peer: Peer) => {
+                console.log('enviando mensaje')
+                p2pt.send(peer, miMensaje)
+            }) 
+        }
+    }, [p2pt, peers])
 
     const clickConnectToGame = useCallback(() => {
         if (address && isConnected) {
-            game.init(
-                address,
-                isConnected,
-                (truco: Truco): void => {
-                    // TODO: este aún no funciona, cuando actualiza no se renderiza nuevamente
-                    setGame(truco)
-                    setInSession(true)
-                }
-            )
+            console.log('p2p started')
+            setInSession(true)
         } else {
             setInSession(false)
         }
         
-    }, [game, isConnected, address, setGame])
+    }, [address, isConnected])
+
+
+    const removePeer = useCallback((disconnectedPeer: Peer) => {
+        setPeers((currentPeers) => currentPeers.filter((peer) => peer === disconnectedPeer))
+        console.log('remove peer', disconnectedPeer)
+    }, [])
+
+    const addPeer = useCallback((newPeer: Peer) => {
+        setPeers((currentPeers) => [...currentPeers, newPeer])
+        console.log('new peer added', newPeer)
+    }, [])
+
+    const trackingConnection = useCallback((tracker:any, stats:any) => {
+        console.log('Connected to tracker : ' + tracker.announceUrl)
+        console.log('Tracker stats : ' + JSON.stringify(stats))
+        console.log('My identifier ', p2pt._peerId)
+    }, [p2pt])
 
     useEffect(() => {
-        clickConnectToGame()
-    }, [clickConnectToGame])
+        if (p2pt && isConnected && inSession) {
+            console.log('go, p2p define')
+            p2pt.on('trackerconnect', (tracker, stats) => {
+                trackingConnection(tracker, stats)
+            })
+            p2pt.on('peerconnect', addPeer)
+            p2pt.on('peerclose', (peer) => {
+                removePeer(peer)
+            })
+            p2pt.on('msg', (peer: Peer, message) => {
+                setMessages((currentMessages) => [...currentMessages, message])
+            })
+            p2pt.start()
+        }
+        return () => { p2pt.destroy() }
+    }, [p2pt, isConnected, inSession])
 
     return {
         address,
         isConnected,
-        game,
-        setGame,
         recoveredAddress,
-        signMessage,
-        data,
         clickConnectToGame,
-        inSession
+        inSession,
+        peers,
+        sendMessageAll,
+        messages
     }
 }
