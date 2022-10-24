670ec5b8479b49d0be67f3c5a04e0f5ec6d03294
Julián Paredes <julpar@users.noreply.github.com>
2022-10-24 11:17:51 -0300

    Solidity structs and first Truco Match contract (#5)
    * reworked structs
    
    * expanded gitignore
    
    * Added general TrucoMatch and make changes to crypt primitives accordingly
    
    * Add UTs for DeckCrypt contract
    
    * Add github action
    
    * rename
    
    * prettier
    
    * change default dir
    
    * change default dir
    
    * change default dir
    
    * change default dir
    
    * Add Truecoin contract
    
    * Update Structs.sol
    
    Make EnvidoCount an Action
    Delete EnvidoEnvido, so we can re-challenge with any kind of envido
    Make Revelead cards been a six position array.
    
    * Update Structs.sol
    
    Co-authored-by: Emiliano Moscato <85498808+emilianomoscato@users.noreply.github.com>

--- protocol/contracts/crypt/DeckCrypt.sol
+++ protocol/contracts/crypt/DeckCrypt.sol
@@ -3,82 +3,61 @@ pragma solidity 0.8.16;
 
 import "../trucoV1/Structs.sol";
 
-contract DeckCrypt  {
-
-    CardsStructs.Deck deck;
-
-    constructor () {
-        createDeck();
-    }
- 
-    function getCardAtId(uint8 id) public view returns (bytes1 card){
-        card  =  deck.cards[id];
-    }
-
-    function getDeck() public view returns (CardsStructs.Deck memory) {
-        return deck;
-    }
-
-    function encryptOrDecryptDeck(string memory _secret) public  {
+contract DeckCrypt {
+    function encryptOrDecryptDeck(
+        CardsStructs.Deck memory _deck,
+        string memory _secret
+    ) public pure returns (CardsStructs.Deck memory) {
         // Generate key using key derivation
-        bytes1[CardsCount] memory key  = keyDerivation(_secret, CardsCount);
-
-        bytes1[CardsCount] memory cards = deck.cards; 
+        bytes1[CardsCount] memory key = keyDerivation(_secret, CardsCount);
 
         for (uint8 i = 0; i < CardsCount; i++) {
-            cards[i] = encryptOrDecryptByte(deck.cards[i], key[i]);
+            _deck.cards[i] = encryptOrDecryptByte(_deck.cards[i], key[i]);
         }
 
-        deck.cards = cards;
+        return _deck;
     }
 
     // Key derivation function: uses a secret and desired lenght and generates a key for each card
-    function keyDerivation(string memory _secret, uint8 _lenght) internal pure returns (bytes1[CardsCount] memory key)
+    function keyDerivation(string memory _secret, uint8 _lenght)
+        internal
+        pure
+        returns (bytes1[CardsCount] memory key)
     {
-        require (bytes(_secret).length != 0, "Secret can't be empty");
-        require(_length != 0)
+        require(bytes(_secret).length > 0, "Secret can't be empty");
 
         uint8 sequence = 0;
         uint8 keyOffSet = 0;
         bytes32 generatedKey = generateKey(_secret, sequence);
-        
-        // We assing this here  to avoid checking special case of division remainder by 0 inside the loop 
-        key[0] = generatedKey[0];
 
-        // Since we set index 0 above we start from 1
-        for (uint8 i = 1; i < _lenght; i++) {
-
-            if (i % 32 == 0) {
+        for (uint8 i = 0; i < _lenght; i++) {
+            if (i > 0 && i % 32 == 0) {
                 sequence++;
-                keyOffSet= sequence * 32;
+                keyOffSet = sequence * 32;
                 generatedKey = generateKey(_secret, sequence);
             }
 
             // Offset keeps pointer relative to actual key byte
-            // i.e: on sequence 1, position 32 will be index 0 of a new obtained secret 
-            key[i] = generatedKey[i-keyOffSet];
+            // i.e: on sequence 1, position 32 will be index 0 of a new obtained secret
+            key[i] = generatedKey[i - keyOffSet];
         }
 
         return key;
     }
 
-    function createDeck() internal {
-
-        bytes1[CardsCount] memory cards = deck.cards; 
-
-        // pack  cards into bytes
-        for (uint8 i = 0; i < CardsCount; i++) {
-            cards[i] = bytes1(i);
-        }
-
-        deck.cards = cards;
-    }
-
-    function generateKey(string memory _secret, uint8 _sequence) internal pure returns (bytes32) {
+    function generateKey(string memory _secret, uint8 _sequence)
+        internal
+        pure
+        returns (bytes32)
+    {
         return keccak256(abi.encodePacked(_secret, _sequence));
     }
 
-    function encryptOrDecryptByte(bytes1 data, bytes1 secret) internal pure returns (bytes1) {
+    function encryptOrDecryptByte(bytes1 data, bytes1 secret)
+        internal
+        pure
+        returns (bytes1)
+    {
         return data ^ secret;
     }
 }
--- protocol/contracts/trucoV1/Structs.sol
+++ protocol/contracts/trucoV1/Structs.sol
@@ -10,29 +10,42 @@ library CardsStructs {
 
     enum Action {
         PlayCard,
+        EnvidoCount,
         Challenge,
         Response,
         Resign
     }
 
     enum Challenge {
+        None,
         Truco,
         ReTruco,
         ValeCuatro,
         Envido,
-        EnvidoEnvido,
         RealEnvido,
         FaltaEnvido
     }
 
     enum Response {
         Accept,
-        DontAccept,
-        EnvidoCount
+        DontAccept
+    }
+
+    struct GameState {
+        int8 playerTurn; // player index
+        int8[] teamPoints; //points indexed by team id (in this case a player is a team)
+        bytes1[6] revealed_cards; // 
+        Challenge currentChallenge;
     }
 
     struct Move {
         Action action;
         bytes1[] parameters;
+    }
+
+    struct Transaction {
+        int8 currentPlayerIndex;
+        GameState state;
+        Move[] moves;
     }
 }
