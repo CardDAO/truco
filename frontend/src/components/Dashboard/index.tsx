1dd6223365a9ccccdbf770315d79d2cfbafd539c
Julian Murphy <njmdistrisoft@gmail.com>
2022-10-26 10:05:53 -0300

    height

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Chat } from "../Chat"
 
 export const Dashboard = ({ peers, messages, messageInput, setMessageInput, sendMessageAll }: any) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-screen grid-flow-row-dense h-1/2">
+        <div className="grid grid-cols-1 grid-rows-2 md:grid-rows-none md:grid-cols-3 gap-4 w-screen">
             <div className="text-center align-middle mx-4 order-2 md:order-1 col-auto">
                 <Chat
                     peers={peers}
@@ -12,8 +12,14 @@ export const Dashboard = ({ peers, messages, messageInput, setMessageInput, send
                     sendMessageAll={sendMessageAll}
                 />
             </div>
-            <div className="bg-gray-800/50 order-1 md:order-2 col-span-2 mx-4">
+            <div className="bg-gray-800/50 order-1 md:order-2 md:col-span-2 mx-4">
                 <div className="m-2">
+                    <p>qweqwe</p>
+                    <p>qweqwe</p>
+                    <p>qweqwe</p>
+                    <p>qweqwe</p>
+                    <p>qwe</p>
+                    <p></p>
                     <p></p>
                 </div>
             </div>
--- frontend/src/components/Root/index.tsx
+++ frontend/src/components/Root/index.tsx
@@ -5,7 +5,7 @@ export const Root = () => {
     return (
         <div className="App">
             <Navbar />
-            <div className="flex items-center justify-center h-[calc(100vh-80px)] overflow-auto">
+            <div className="flex items-center justify-center h-[calc(100vh-20vh)]">
                 <Outlet />
             </div>
         </div>
