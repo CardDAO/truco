e94bd1c1eaa8edcfd9127446361d1fcea7b1753e
Julian Paredes <jparedes@gmail.com>
2022-10-27 11:08:18 -0300

    Add Truco first implementation and UTs

--- protocol/contracts/trucoV1/resolvers/TrucoResolver.sol
+++ protocol/contracts/trucoV1/resolvers/TrucoResolver.sol
@@ -4,44 +4,106 @@ pragma solidity 0.8.16;
 import "../Structs.sol";
 
 /**
-* TrucoResolver: 
-* Solidity can't apply interfaces on libraries, but all resolvers should follow the following interface:
-*
-*    function resolve(
-*       CardsStructs.GameState memory _gameState,
-*        CardsStructs.Move memory _move
-*    ) internal returns (CardsStructs.GameState memory); 
-*
-*    function canResolve(
-*        CardsStructs.Challenge _challenge
-*    ) internal pure returns (bool);
-*
-*    function isFinal(CardsStructs.GameState memory _gameState) internal pure returns (bool) 
-*    
-*    function getWinner(CardsStructs.GameState memory _gameState) internal pure returns (uint8) 
-*
-* Attention: Only internal functions should be used in this library
-*/
+ * TrucoResolver:
+ * Solidity can't apply interfaces on libraries, but all resolvers should follow the following interface:
+ *
+ *    function resolve(
+ *       CardsStructs.GameState memory _gameState,
+ *        CardsStructs.Move memory _move
+ *    ) internal returns (CardsStructs.GameState memory);
+ *
+ *    function canResolve(
+ *        CardsStructs.Challenge _challenge
+ *    ) internal pure returns (bool);
+ *
+ *    function isFinal(CardsStructs.GameState memory _gameState) internal pure returns (bool)
+ *
+ *    function getWinner(CardsStructs.GameState memory _gameState) internal pure returns (uint8)
+ *
+ * Attention: Only internal functions should be used in this library
+ */
 library TrucoResolver {
     function resolve(
         CardsStructs.GameState memory _gameState,
         CardsStructs.Move memory _move
     ) internal returns (CardsStructs.GameState memory) {
         // Valid moves up to this point are enforced before any logic
+        require(
+            _move.action == CardsStructs.Action.Challenge ||
+                _move.action == CardsStructs.Action.Response ||
+                _move.action == CardsStructs.Action.PlayCard
+        );
 
-        if (_move.action == CardsStructs.Action.Challenge) {
-            _gameState.currentChallenge.waitingChallengeResponse = true;
-            _gameState.currentChallenge.challenge = CardsStructs.Challenge(
-                _move.parameters[0]
+        // When current challenge is at a refusal state, no new moves can be processed
+        if (
+            _gameState.currentChallenge.challenge !=
+            CardsStructs.Challenge.None &&
+            _gameState.currentChallenge.response == CardsStructs.Response.Refuse
+        ) {
+            revert(
+                "No new moves can be processed while current challenge is at refusal state"
             );
         }
 
-        return _gameState;
+        // Check whether the game is at a final state and should not accept any new moves
+        if (isFinal(_gameState)) {
+            revert("Game is in final state");
+        }
+
+        // ---------------------------------------------------------------------------------------------------------
+        // Main Logic:
+        // 3 possible cases are handled:
+
+        // 2) Check if action is an envido acceptance or refusal
+        if (_move.action == CardsStructs.Action.Response) {
+            // Preconditions:
+            // - Challenge should have no response
+            // - Can't be the challenger
+            // - Should be a valid response
+            require(
+                _gameState.currentChallenge.response ==
+                    CardsStructs.Response.None
+            );
+
+            CardsStructs.Response response = CardsStructs.Response(
+                _move.parameters[0]
+            );
+
+            require(
+                response == CardsStructs.Response.Accept ||
+                    response == CardsStructs.Response.Refuse
+            );
+
+            require(
+                _gameState.playerTurn != _gameState.currentChallenge.challenger
+            );
+
+            _gameState.currentChallenge.waitingChallengeResponse = false;
+            _gameState.currentChallenge.response = response;
+
+            // If response is a refusal, game ends
+            if (response == CardsStructs.Response.Refuse) {
+                return _gameState;
+            }
+
+            // If it's an acceptance, points are overriden with the challenge points
+            _gameState.currentChallenge.pointsAtStake = pointsPerChallenge(
+                _gameState.currentChallenge.challenge,
+                _gameState
+            );
+
+            return _gameState;
+        }
+
+        // If we reach this point, it means that the move is not valid
+        revert("Invalid Move");
     }
 
-    function canResolve(
-        CardsStructs.Challenge _challenge
-    ) internal pure returns (bool) {
+    function canResolve(CardsStructs.Challenge _challenge)
+        internal
+        pure
+        returns (bool)
+    {
         if (
             _challenge == CardsStructs.Challenge.None ||
             _challenge == CardsStructs.Challenge.Truco ||
@@ -53,14 +115,42 @@ library TrucoResolver {
 
         return false;
     }
-    
-    function isFinal(CardsStructs.GameState memory _gameState) internal pure returns (bool) {
+
+    function isFinal(CardsStructs.GameState memory _gameState)
+        internal
+        pure
+        returns (bool)
+    {
         // TODO: Implement this
         return false;
     }
 
-    function getWinner(CardsStructs.GameState memory _gameState) internal pure returns (uint8) {
+    function getWinner(CardsStructs.GameState memory _gameState)
+        internal
+        pure
+        returns (uint8)
+    {
         // TODO: Implement this
         return _gameState.playerTurn;
+    }
+
+    // Return points that should be at stake for a given challenge
+    function pointsPerChallenge(
+        CardsStructs.Challenge challenge,
+        CardsStructs.GameState memory _gameState
+    ) internal pure returns (uint8) {
+        if (challenge == CardsStructs.Challenge.Truco) {
+            return 2;
+        }
+
+        if (challenge == CardsStructs.Challenge.ReTruco) {
+            return 3;
+        }
+
+        if (challenge == CardsStructs.Challenge.ValeCuatro) {
+            return 4;
+        }
+
+        revert("Invalid challenge");
     }
 }
---
+++ protocol/test/trucoV1/truco.ts
@@ -0,0 +1,261 @@
+import { expect } from "chai"
+import { ethers } from "hardhat"
+
+import { CardsStructs } from "../../typechain-types/contracts/trucoV1/Engine"
+import { ActionEnum, ChallengeEnum, ResponseEnum } from "./struct-enums"
+
+import MoveStruct = CardsStructs.MoveStruct
+import TransactionStruct = CardsStructs.TransactionStruct
+import GameStateStruct = CardsStructs.GameStateStruct
+
+import { BigNumber } from "ethers"
+
+describe("Truco Resolver", function () {
+  const currentPlayerIdx = BigNumber.from(0)
+  const otherPlayerIdx = BigNumber.from(1)
+
+  async function deployContract() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner] = await ethers.getSigners()
+
+    const Trucoin = await ethers.getContractFactory("Trucoin")
+    const truecoin = await Trucoin.deploy()
+
+    const TrucoEngine = await ethers.getContractFactory("EngineTester")
+    const sut = await TrucoEngine.deploy(truecoin.address)
+
+    return { sut, truecoin, owner }
+  }
+
+  function basicGameState(): GameStateStruct {
+    return {
+      playerTurn: BigNumber.from(currentPlayerIdx),
+      playerWhoShuffled: BigNumber.from(currentPlayerIdx),
+      pointsToWin: BigNumber.from(0),
+      currentChallenge: {
+        challenge: BigNumber.from(ChallengeEnum.None),
+        challenger: BigNumber.from(currentPlayerIdx),
+        pointsAtStake: BigNumber.from(currentPlayerIdx),
+        waitingChallengeResponse: false,
+        response: BigNumber.from(ResponseEnum.None),
+      },
+      revealedCardsByPlayer: [
+        [BigNumber.from(0), BigNumber.from(0)],
+        [BigNumber.from(0), BigNumber.from(0)],
+        [BigNumber.from(0), BigNumber.from(0)],
+      ],
+      envidoCountPerPlayer: [BigNumber.from(0), BigNumber.from(0)],
+      teamPoints: [BigNumber.from(0), BigNumber.from(0)],
+    }
+  }
+
+  describe("Invalid moves", function () {})
+
+  /**
+   * Precondintion: No challenge in place
+   */
+  describe("No previous challenge", function () {})
+
+  /**
+   * Precondintion: A challenge is in place but is not yet accepted (or raised)
+   */
+  describe("Challenge in place: waiting for a response", function () {
+    // Envido has spelled by current player and accepted by other party
+    function basicGameStateWithTrucoSpellWaiting(): GameStateStruct {
+      let state: GameStateStruct = basicGameState()
+
+      // Envido has spelled by other player and accepted, cant respond anymore
+      state.currentChallenge.challenge = BigNumber.from(ChallengeEnum.Truco)
+      state.currentChallenge.challenger = BigNumber.from(otherPlayerIdx)
+      state.currentChallenge.waitingChallengeResponse = true
+      state.currentChallenge.pointsAtStake = BigNumber.from(1)
+      state.currentChallenge.response = BigNumber.from(ResponseEnum.None)
+
+      return state
+    }
+
+    it("Invalid response: 'None' is not accepted as a challenge response", async function () {
+      const { sut } = await deployContract()
+
+      let state: GameStateStruct = basicGameStateWithTrucoSpellWaiting()
+
+      // Truco has spelled by other player but not yet accepted yet
+      state.currentChallenge.waitingChallengeResponse = true
+      state.currentChallenge.response = BigNumber.from(ResponseEnum.None)
+
+      let move: MoveStruct = {
+        action: BigNumber.from(ActionEnum.Response),
+        parameters: [BigNumber.from(ResponseEnum.None)],
+      }
+
+      let transaction: TransactionStruct = {
+        playerIdx: state.playerTurn,
+        moves: [move],
+        state: state,
+      }
+
+      await expect(sut.executeTransaction(transaction)).to.be.reverted
+    })
+
+    it("PlayCard shouldn't be allowed without a valid response first", async function () {
+      const { sut } = await deployContract()
+
+      let state: GameStateStruct = basicGameStateWithTrucoSpellWaiting()
+
+      let move: MoveStruct = {
+        action: BigNumber.from(ActionEnum.PlayCard),
+        parameters: [],
+      }
+
+      let transaction: TransactionStruct = {
+        playerIdx: state.playerTurn,
+        moves: [move],
+        state: state,
+      }
+
+      await expect(sut.executeTransaction(transaction)).to.be.reverted
+    })
+
+    it("Truco casted by current player and it's not the one who should respond", async function () {
+      const { sut } = await deployContract()
+
+      let state: GameStateStruct = basicGameStateWithTrucoSpellWaiting()
+      state.currentChallenge.challenger = BigNumber.from(currentPlayerIdx)
+
+      let move: MoveStruct = {
+        action: BigNumber.from(ActionEnum.Response),
+        parameters: [BigNumber.from(ResponseEnum.Accept)],
+      }
+
+      let transaction: TransactionStruct = {
+        playerIdx: state.playerTurn,
+        moves: [move],
+        state: state,
+      }
+
+      await expect(sut.executeTransaction(transaction)).to.be.reverted
+    })
+
+    it("Refuse challenge", async function () {
+      const { sut } = await deployContract()
+
+      let state: GameStateStruct = basicGameStateWithTrucoSpellWaiting()
+
+      let move: MoveStruct = {
+        action: BigNumber.from(ActionEnum.Response),
+        parameters: [BigNumber.from(ResponseEnum.Refuse)],
+      }
+
+      let transaction: TransactionStruct = {
+        playerIdx: state.playerTurn,
+        moves: [move],
+        state: state,
+      }
+
+      await sut.executeTransaction(transaction)
+
+      let result: GameStateStruct = await sut.gameState()
+
+      // Check resulting state
+      expect(result.currentChallenge.waitingChallengeResponse).to.be.false
+      expect(result.currentChallenge.response).to.be.equal(
+        BigNumber.from(ResponseEnum.Refuse)
+      )
+      expect(result.currentChallenge.pointsAtStake).to.be.equal(
+        BigNumber.from(1)
+      )
+    })
+
+    it("Accept challenge", async function () {
+      const { sut } = await deployContract()
+
+      let state: GameStateStruct = basicGameStateWithTrucoSpellWaiting()
+
+      let move: MoveStruct = {
+        action: BigNumber.from(ActionEnum.Response),
+        parameters: [BigNumber.from(ResponseEnum.Accept)],
+      }
+
+      let transaction: TransactionStruct = {
+        playerIdx: state.playerTurn,
+        moves: [move],
+        state: state,
+      }
+
+      await sut.executeTransaction(transaction)
+
+      let result: GameStateStruct = await sut.gameState()
+
+      // Check resulting state
+      expect(result.currentChallenge.waitingChallengeResponse).to.be.false
+      expect(result.currentChallenge.response).to.be.equal(
+        BigNumber.from(ResponseEnum.Accept)
+      )
+      expect(result.currentChallenge.pointsAtStake).to.be.equal(
+        BigNumber.from(2)
+      )
+      expect(result.currentChallenge.challenger).to.be.equal(otherPlayerIdx)
+    })
+  })
+
+  /**
+   * Precondintion: A challenge was REFUSED
+   */
+  describe("Challenge in place: Refused", function () {
+    // Envido has spelled by current player and accepted by other party
+    function basicGameStateWithEnvidoSpellRefused(): GameStateStruct {
+      let state: GameStateStruct = basicGameState()
+
+      state.currentChallenge.challenge = BigNumber.from(ChallengeEnum.Envido)
+      state.currentChallenge.challenger = BigNumber.from(otherPlayerIdx)
+      state.currentChallenge.waitingChallengeResponse = false
+      state.currentChallenge.response = BigNumber.from(ResponseEnum.Refuse)
+      state.currentChallenge.pointsAtStake = BigNumber.from(2)
+
+      return state
+    }
+  })
+  /**
+   * Precondintion: A challenge was ACCEPTED
+   */
+  describe("Challenge in place: Accepted", function () {
+    // Envido has spelled by current player and accepted by other party
+    function basicGameStateWithEnvidoSpell(): GameStateStruct {
+      let state: GameStateStruct = basicGameState()
+
+      state.playerWhoShuffled = otherPlayerIdx
+      state.currentChallenge.challenge = BigNumber.from(ChallengeEnum.Envido)
+      state.currentChallenge.challenger = BigNumber.from(currentPlayerIdx)
+      state.currentChallenge.waitingChallengeResponse = false
+      state.currentChallenge.response = BigNumber.from(ResponseEnum.Accept)
+      state.currentChallenge.pointsAtStake = BigNumber.from(2)
+
+      return state
+    }
+    describe("Out of order moves", function () {})
+
+    describe("Raising the challenge", function () {})
+
+    describe("Points count stage", function () {})
+  })
+  /**
+   * Precondition: A challenge is finished (all players have cast their envido count)
+   */
+  describe("Challenge is finished", function () {
+    // Envido has spelled by current player and accepted by other party
+    function basicGameStateWithEnvidoSpellFinished(): GameStateStruct {
+      let state: GameStateStruct = basicGameState()
+
+      // Envido has spelled by other player and accepted, cant respond anymore
+      state.currentChallenge.challenge = BigNumber.from(ChallengeEnum.Envido)
+      state.currentChallenge.challenger = BigNumber.from(otherPlayerIdx)
+      state.currentChallenge.waitingChallengeResponse = false
+      state.currentChallenge.pointsAtStake = BigNumber.from(2)
+      state.currentChallenge.response = BigNumber.from(ResponseEnum.Accept)
+
+      return state
+    }
+
+    describe("Compute the winner", function () {})
+  })
+})
