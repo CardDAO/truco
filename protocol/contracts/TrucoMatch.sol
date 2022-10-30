ae1a8a0d32a7cd8fe5220a4d09fb23a5068ab9dc
Emiliano Moscato <emiliano.moscato@satellogic.com>
2022-10-30 17:47:16 -0300

    Construct, join, stake with tests

--- protocol/contracts/TrucoMatch.sol
+++ protocol/contracts/TrucoMatch.sol
@@ -9,89 +9,117 @@ import "./crypt/DeckCrypt.sol";
 
 contract TrucoMatch  {
 
+    struct player {
+        address playerAddress;
+        uint256 tokensAtStake;
+    }
+    
     struct Match {
-        address[2] players; // player 0 is the creator of the match
+        player[2] players; // player 0 is the creator of the match
         CardsStructs.Deck deck;
         CardsStructs.GameState gameState;
         uint256 tokensAtStake; 
+
     }
 
     Engine trucoEngine;
     DeckCrypt deckCrypt;
-    IERC20 trueCoin;
+    IERC20 truCoin;
     Match currentMatch;
 
     // Events
-    event MatchCreated(address player, uint256 tokensAtStake);
-    event MatchStarted(address[2] players, uint256 tokensAtStake);
+    event MatchCreated(address match_address);
+    event MatchStarted(address player1, address player2, uint256 tokensAtStake);
+    event PlayerStaked(address player, uint256 tokensAtStake);
     event DealStarted(address shuffler);
     event DealEnded();
 
 
-    constructor (Engine _trucoEngine, DeckCrypt _deckCrypt, IERC20 _truecoin, uint256 _tokensAtStake) {
+    constructor (Engine _trucoEngine, DeckCrypt _deckCrypt, IERC20 _truCoin, uint256 _tokensAtStake) {
         trucoEngine = _trucoEngine;
         deckCrypt = _deckCrypt;
-        trueCoin = _truecoin;
-
-        //Transfer Trucoins from owner to contract
-        trueCoin.transferFrom(msg.sender, address(this), _tokensAtStake);
+        truCoin = _truCoin;
         currentMatch.tokensAtStake = _tokensAtStake;
-
-        currentMatch.players[0] = msg.sender;
+        currentMatch.players[0].playerAddress = msg.sender;
 
         //Mint Sould Bound Token
 
-        emit MatchCreated(msg.sender, _tokensAtStake);
+        emit MatchCreated(address(this));
     }
-    
         
-    // Method for joining the match 
+    // Method for second player joining the match 
     function join() public {
-        require(currentMatch.players[1] == address(0), "Match is full");
-        require(msg.sender != currentMatch.players[0], "You can't join your own match");
-        require(trueCoin.balanceOf(msg.sender) >= currentMatch.tokensAtStake, "You don't have enough Trucoins to join this match");
+        require(currentMatch.players[0].playerAddress != msg.sender, "Match creator is already joined");
+        require(currentMatch.players[1].playerAddress == address(0), "Match is full");
+        require(truCoin.balanceOf(msg.sender) >= currentMatch.tokensAtStake, "You don't have enough Trucoins to join this match");
+        require(truCoin.allowance(msg.sender, address(this)) >= currentMatch.tokensAtStake, "Not enough trucoins transfer approved");
 
         // Check invitations if any
 
-        // Transfer Trucoins from player to contract
-        trueCoin.transferFrom(msg.sender, address(this), currentMatch.tokensAtStake);
-        currentMatch.tokensAtStake += currentMatch.tokensAtStake;
+        currentMatch.players[1].playerAddress = msg.sender;
 
-        currentMatch.players[1] = msg.sender;
-
-        // Start match
-        currentMatch.gameState = trucoEngine.startMatch();
-        emit MatchStarted(currentMatch.players, currentMatch.tokensAtStake);
+        // Make the user stake so he can not kidnap the match
+        stake(1);
     }
 
-    function newDeal(CardsStructs.Deck memory _deck) {
+    // Method for staking & starting the match
+    function stake(uint8 _player) public {
+        require(
+            currentMatch.players[_player].playerAddress == msg.sender, 
+            "Player is not joined");
+        require(
+            truCoin.balanceOf(msg.sender) >= currentMatch.tokensAtStake, 
+            "You don't have enough Trucoins to stake this match");
+        require(
+            truCoin.allowance(msg.sender, address(this)) >= currentMatch.tokensAtStake, 
+            "Not enough trucoins transfer approved");
+        require(
+            currentMatch.players[_player].tokensAtStake == 0, 
+            "You already staked this match");
+
+        // Transfer Trucoins from player to contract
+        require(truCoin.transferFrom(
+            msg.sender, address(this), currentMatch.tokensAtStake), 
+            "Trucoin transfer failed");
+        
+        currentMatch.players[_player].tokensAtStake = currentMatch.tokensAtStake;
+
+        // emit event
+        emit PlayerStaked(msg.sender, currentMatch.tokensAtStake);
+
+        // Start match
+        if (currentMatch.players[0].tokensAtStake == currentMatch.tokensAtStake && 
+            currentMatch.players[1].tokensAtStake == currentMatch.tokensAtStake) {
+            currentMatch.gameState = trucoEngine.startGame();
+            emit MatchStarted(
+                currentMatch.players[0].playerAddress, 
+                currentMatch.players[1].playerAddress,
+                currentMatch.tokensAtStake);
+        }
+    }
+
+    function newDeal(CardsStructs.Deck memory _deck) public {
         // Check if current game state enables new card shufflings
         require(!currentMatch.gameState.isDealOpen, "Deal is already open");
 
         // Determine the new shuffler and check that corresponds to the current player
-        uint8 new_shuffler = currentMatch.playerWhoShuffled ^ 1;
-        require(msg.sender == currentMatch.players[new_shuffler], "You are not the shuffler");
+        uint8 new_shuffler = currentMatch.gameState.playerWhoShuffled ^ 1;
+        require(msg.sender == currentMatch.players[new_shuffler].playerAddress, "You are not the shuffler");
 
         currentMatch.deck = _deck; // encrypted deck using 2 secrets
-
-        // Assign cards for each player interleaving one for each other, i.e player1: 0,2,4 player2: 1,3,5
-        for (uint8 i = 0; i < 3; i++) {
-            currentMatch.gameState.playerCards[0][i] = i*2;
-            currentMatch.gameState.playerCards[1][i] = i*2 + 1;
-        }
         
         // Assign new shuffler
-        currentMatch.playerWhoShuffled = new_shuffler;
+        currentMatch.gameState.playerWhoShuffled = new_shuffler;
 
         // Assign turn to player not shuffling
-        currentMatch.gameState.playerTurn = currentMatch.playerWhoShuffled ^ 1;
+        currentMatch.gameState.playerTurn = currentMatch.gameState.playerWhoShuffled ^ 1;
 
         // Unset current challenge from previous deal
         currentMatch.gameState.currentChallenge.challenge = CardsStructs.Challenge.None;
 
         // Clean revealed cards
-        currentMatch.gameState.revealedCardsByPlayer[0] = new uint8[3](0);
-        currentMatch.gameState.revealedCardsByPlayer[1] = new uint8[3](0);
+        currentMatch.gameState.revealedCardsByPlayer[0] = [0,0,0];
+        currentMatch.gameState.revealedCardsByPlayer[1] = [0,0,0];
 
         // Clean envido count
         currentMatch.gameState.envidoCountPerPlayer[0] = 0;
@@ -100,9 +128,13 @@ contract TrucoMatch  {
         // Set deal as open
         currentMatch.gameState.isDealOpen = true;
 
-        emit DealStarted(msg.sender, currentMatch.tokensAtStake);
+        emit DealStarted(msg.sender);
     }
 
+    // Get players addresses
+    function getPlayers() public view returns (address[2] memory) {
+        return [currentMatch.players[0].playerAddress, currentMatch.players[1].playerAddress];
+    }
 
     // [Test Functions] --------------------------------------------------------------
     
