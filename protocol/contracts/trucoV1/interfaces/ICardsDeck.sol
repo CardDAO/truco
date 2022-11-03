7f4d59a68588d1a9572eb1b3a99ee56c7b4f04ec
Julian Paredes <jparedes@gmail.com>
2022-11-03 17:02:35 -0300

    Prettier

--- protocol/contracts/trucoV1/interfaces/ICardsDeck.sol
+++ protocol/contracts/trucoV1/interfaces/ICardsDeck.sol
@@ -2,13 +2,17 @@
 pragma solidity 0.8.16;
 
 interface ICardsDeck {
-
     struct Card {
         uint8 value;
         uint8 suit;
     }
 
     function suitName(uint8 _card) external view returns (string memory);
-    function areSameSuit(uint8 _card1, uint8 _card2)  external view returns (bool);
+
+    function areSameSuit(uint8 _card1, uint8 _card2)
+        external
+        view
+        returns (bool);
+
     function decodeCard(uint8 _card) external view returns (Card memory);
 }
