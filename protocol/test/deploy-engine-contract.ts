c7e3db6212572fa7ef57c472eca37106088ff2b0
Julian Paredes <jparedes@gmail.com>
2022-11-03 17:06:47 -0300

    Prettier

--- protocol/test/deploy-engine-contract.ts
+++ protocol/test/deploy-engine-contract.ts
@@ -1,32 +1,32 @@
 const { ethers } = require("hardhat");
 
 export async function deployEngineContract() {
-    const Trucoin = await ethers.getContractFactory("Trucoin");
-    const trucoin = await Trucoin.deploy();
+  const Trucoin = await ethers.getContractFactory("Trucoin");
+  const trucoin = await Trucoin.deploy();
 
-    const TrucoResolver = await ethers.getContractFactory("TrucoResolver");
-    const trucoResolver = await TrucoResolver.deploy();
+  const TrucoResolver = await ethers.getContractFactory("TrucoResolver");
+  const trucoResolver = await TrucoResolver.deploy();
 
-    const EnvidoResolver = await ethers.getContractFactory("EnvidoResolver");
-    const envidoResolver = await EnvidoResolver.deploy();
+  const EnvidoResolver = await ethers.getContractFactory("EnvidoResolver");
+  const envidoResolver = await EnvidoResolver.deploy();
 
-    const CardsDeck = await ethers.getContractFactory("CastillianDeck");
-    const cardsDeck = await CardsDeck.deploy();
+  const CardsDeck = await ethers.getContractFactory("CastillianDeck");
+  const cardsDeck = await CardsDeck.deploy();
 
-    const EngineQueries = await ethers.getContractFactory("EngineQueries");
-    const engineQueries = await EngineQueries.deploy(
-        trucoResolver.address,
-        envidoResolver.address,
-        cardsDeck.address
-    );
+  const EngineQueries = await ethers.getContractFactory("EngineQueries");
+  const engineQueries = await EngineQueries.deploy(
+    trucoResolver.address,
+    envidoResolver.address,
+    cardsDeck.address
+  );
 
-    const TrucoEngine = await ethers.getContractFactory("EngineTester");
-    const engine = await TrucoEngine.deploy(
-        trucoin.address,
-        trucoResolver.address,
-        envidoResolver.address,
-        engineQueries.address
-    );
+  const TrucoEngine = await ethers.getContractFactory("EngineTester");
+  const engine = await TrucoEngine.deploy(
+    trucoin.address,
+    trucoResolver.address,
+    envidoResolver.address,
+    engineQueries.address
+  );
 
   return { engine, trucoin };
 }
