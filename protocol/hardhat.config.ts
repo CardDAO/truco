98115bdf6f4bd0a56cbbe983c181a6309c42bede
Julian Paredes <jparedes@gmail.com>
2022-11-09 11:30:02 -0300

    Enable solc optimizer

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -11,7 +11,12 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.16"
+        version: "0.8.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
       },
     ],
   },
