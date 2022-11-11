e5b5d9374eceea9d3e33cb5b9925312af14b489e
Julian Murphy <njmdistrisoft@gmail.com>
2022-11-10 23:37:23 -0300

    main script

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -8,11 +8,22 @@ import { deployTrucoinContract } from "./helpers/trucoin-deploy"
 
 async function main() {
     const { trucoin } = await deployTrucoinContract()
+    console.log(`Trucoin deployed in address: ${trucoin.address}`)
+
     const { trucoResolver } = await deployTrucoResolverContract()
+    console.log(`TrucoResolver deployed in address: ${trucoResolver.address}`)
+
     const { envidoResolver } = await deployEnvidoResolverContract()
+    console.log(`EnvidoResolver deployed in address: ${envidoResolver.address}`)
+
     const { cardsDeck } = await deployDeckContract()
+    console.log(`Deck deployed in address: ${cardsDeck.address}`)
+
     const { gameStateQueries } = await deployGameStateQueriesContract(cardsDeck, envidoResolver, trucoResolver)
+    console.log(`GameStateQueries deployed in address: ${gameStateQueries.address}`)
+
     const { engine } = await deployEngineContract(trucoin, trucoResolver, envidoResolver, gameStateQueries)
+    console.log(`Engine deployed in address: ${engine.address}`)
 
 }
 
