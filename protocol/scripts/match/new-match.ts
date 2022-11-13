d2cfd7db53a79d9560bbe0decf15d3532ba25165
Julian Murphy <njmdistrisoft@gmail.com>
2022-11-13 04:56:42 -0300

    manual create match

--- protocol/scripts/match/new-match.ts
+++ protocol/scripts/match/new-match.ts
@@ -4,16 +4,23 @@ import "@nomiclabs/hardhat-ethers";
 export const deployNewMatch = async (_, { ethers }) => {
     const MatchFactory = await ethers.getContractFactory("TrucoMatchFactory") 
     const matchFactory = await MatchFactory.attach(_.factory) 
+    // TODO use param
     const [player1, player2, player3] = await ethers.getSigners()
 
     const Trucoin = await ethers.getContractFactory("Trucoin") 
     const trucoin = await Trucoin.attach(_.trucoin) 
     const amount = _.amount
 
-    await trucoin.connect(player3).approve(_.factory.address, amount)
+    console.log(`Approve Trucoin amount ${amount} from ${player3.address} to TrucoMatchFactory...`)
+    await trucoin.connect(player3).approve(_.factory, amount)
 
-    await matchFactory.connect(player3).newMatch(amount)
+    console.log(`Deploy match...`)
+    const tx = await matchFactory.connect(player3).newMatch(amount)
+    const { events } = await tx.wait()
+    const event = events.find(
+        (e: { event: string }) => e.event === 'TrucoMatchCreated'
+    )
+    const match_address = event.args['match_address']
 
-   console.log(`Deployed match...`)
-   // TODO no deploya aún
+    console.log(`Deployed success. Match address: ${match_address}`)
 }
