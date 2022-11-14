9fee0d46e13d5ed969b6641c7c9ce66954474055
Julian Murphy <njmdistrisoft@gmail.com>
2022-11-14 02:02:36 -0300

    init button

--- frontend/src/components/Game/index.tsx
+++ frontend/src/components/Game/index.tsx
@@ -1,7 +1,10 @@
+import { ethers } from "ethers"
 import { useEffect, useState, useCallback, KeyboardEventHandler} from 'react'
 import { useTruco } from "../../engine/truco/useTruco"
 import {  useAccountInformation, AccountType, useCreateRoom, Player, Message, sendMessage } from '../../hooks/providers/Wagmi'
 import { Dashboard } from "../Dashboard"
+
+const BUTTON_STYLE = "text-white focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
 
 export const Game = () => {
 
@@ -9,7 +12,19 @@ export const Game = () => {
     const [matchAddress, setMatchAddress] = useState("")
     const { address, isConnected } : AccountType = useAccountInformation()
     const [ inSession, setInSession ]  = useState(false)
+    const [ invalidAddress, setInvalidAddress ] = useState("")
 
+    const verifyAndSetMatchAddress = (e: any) => {
+        try {
+            if (e.target.value.length > 0) {
+                ethers.utils.getAddress(e.target.value)
+            }
+            setInvalidAddress("")
+            setMatchAddress(e.target.value)
+        } catch {
+            setInvalidAddress("Error, verifique el address del match")
+        }
+    }
 
     return (
         <>
@@ -20,8 +35,11 @@ export const Game = () => {
                     !inSession ? 
                     <div className="text-center align-middle">
                         <h2 className="text-white text-xl py-5 text-center align-middle">Start game {address?.toString()}</h2>
-                        <input type="text" onChange={(e) => setMatchAddress(e.target.value)}/>
-                        <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={() => { setInSession(!inSession) }}>Join game</button>
+                        <div>
+                            <input placeholder="Match Addresss (empty to deploy)" type="text" onChange={verifyAndSetMatchAddress} className="block p-2 w-full rounded-lg border sm:text-xs bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 my-5" />
+                        </div>
+                        <button type="button" disabled={invalidAddress? true : false} className={ invalidAddress ? BUTTON_STYLE + " bg-gray-400" : BUTTON_STYLE + " bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl" } onClick={() => { setInSession(!inSession) }}>Join game</button>
+                        <p className="text-sm text-red-500">{invalidAddress ?? ""}</p>
                     </div>
                     :
                     <Dashboard
