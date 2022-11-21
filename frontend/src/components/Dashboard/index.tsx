7dd8b52d454dfa8dd1c1289b310872b9f1fbd3e7
Julian Murphy <njmdistrisoft@gmail.com>
2022-11-21 17:34:33 -0300

    style

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -243,7 +243,7 @@ export const Dashboard = ({ address, inSession, matchAddress }: any) => {
                 <p>Deck: {deck}</p>
                 <p>cardcodewords: {cardCodewords}</p>
                 <p>status: {state}</p>
-                <button type="button" className="text-white focus:ring-4 focus:outline-none font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800" onClick={()=> { setProcessingAction(true); setTimeout(() => {setProcessingAction(false)}, 2000) }}>
+                <button type="button" className={`Loading-GameState ${processingAction ? "animate-bounce bg-indigo-500 shadow-lg shadow-indigo-500/50" : "" }`} onClick={()=> { setProcessingAction(true); setTimeout(() => {setProcessingAction(false)}, 2000) }}>
                     {
                     processingAction ?
                     <svg aria-hidden="true" role="status" className="inline w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
